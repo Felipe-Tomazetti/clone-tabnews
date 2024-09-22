ce144ecbb5a06df8248a891b455d6bb0db5d3e67
Felipe Pistori Tomazetti <48369245+Felipe-Tomazetti@users.noreply.github.com>
2024-09-22 15:35:25 +0000

    adiciona foto do casamento

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
+import casamentoIMG from './casamento.jpg'
+
 function Home () {
-    return <h1>Flávia, Eu te mais que tudo meu amor. Se você me ama, dá uma risadinha. 😎</h1>
+    console.log('img', casamentoIMG)
+    return <div>
+        <h1>Flávia, Eu te amo mais que tudo meu amor. Se você me ama, dá uma risadinha. 😎</h1>
+        <img src={casamentoIMG.src} width='850' height='600'/>
+    </div>
 }
 
 export default Home;
