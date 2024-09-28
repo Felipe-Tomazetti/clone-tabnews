08e4efc240384f05fb2f87b6c85e3140391990e7
Felipe Pistori Tomazetti <48369245+Felipe-Tomazetti@users.noreply.github.com>
2024-09-28 15:53:01 +0000

    adiciona scripts `link:check` e `link:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,16 @@
-import casamentoIMG from './casamento.jpg'
+import casamentoIMG from "./casamento.jpg";
 
-function Home () {
-    console.log('img', casamentoIMG)
-    return <div>
-        <h1>Flávia, Eu te amo mais que tudo meu amor. Se você me ama, dá uma risadinha. 😎</h1>
-        <img src={casamentoIMG.src} width='850' height='600'/>
+function Home() {
+  console.log("img", casamentoIMG);
+  return (
+    <div>
+      <h1>
+        Flávia, Eu te amo mais que tudo meu amor. Se você me ama, dá uma
+        risadinha. 😎
+      </h1>
+      <img src={casamentoIMG.src} width="850" height="600" />
     </div>
+  );
 }
 
 export default Home;
